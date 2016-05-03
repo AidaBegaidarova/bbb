44e8b84066274404885689a8555a3f4368261f36
milton14 <milton14@rambler.ru>
2016-05-03 22:45:19 +0400

    Update task_1_6_7.py

--- task_1_6_7.py
+++ task_1_6_7.py
@@ -3,11 +3,12 @@ import socket, os
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("127.0.0.1", 2222))
 s.listen(10)
-pid=os.fork()
-if pid==0:
-    while True:
-        conn, addr = s.accept()
-        data = conn.recv(1024)
-        conn.send(data)
-        print(data)
-        conn.close()
+for i in range(10):
+    os.fork()
+    if pid==0:
+        while True:
+            conn, addr = s.accept()
+            data = conn.recv(1024)
+            conn.send(data)
+            print(data)
+            conn.close()
