1c56ff63c412f3d9aaf10309ba597d5d94bbe021
milton14 <milton14@rambler.ru>
2016-05-03 22:30:41 +0400

    Update task_1_6_7.py

--- task_1_6_7.py
+++ task_1_6_7.py
@@ -1,15 +1,13 @@
-import socket
+import socket, os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("127.0.0.1", 2222))
 s.listen(10)
-while True:
-    conn, addr = s.accept()
+os.fork()
+if pid==0:
     while True:
+        conn, addr = s.accept()
         data = conn.recv(1024)
-        if not data: break
-        if data == "close":
-            conn.close()
-            break
         conn.send(data)
         print(data)
+        conn.close()
