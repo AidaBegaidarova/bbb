1e612293cae6dbc2f0ed42ac3dd83523f01e1d37
milton14 <milton14@rambler.ru>
2016-05-05 20:19:11 +0300

    task_1_9_12

--- init.sh
+++ init.sh
@@ -2,4 +2,5 @@ ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 
 ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
+ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/task_1_9_12
 /etc/init.d/gunicorn restart
