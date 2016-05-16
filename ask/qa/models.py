ca9fa9abdcfe38479e83a14682c25372009c5181
milton14 <milton14@rambler.ru>
2016-05-16 16:22:33 +0300

    task_2_3_10

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(User,default=1)
+    likes = models.ManyToManyField(User, related_name='questions', blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('question', kwargs={"id": self.id})
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, default=1)
--- init.sh
+++ init.sh
@@ -5,3 +5,6 @@ ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/task_1_9_12
 ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 /etc/init.d/gunicorn restart
+
+/etc/init.d/mysql start
+mysql -uroot -e "create database qa"
