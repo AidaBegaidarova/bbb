ff2e8fe288eea7a4a134434145415b27cbc9d20d
milton14 <milton14@rambler.ru>
2016-05-18 11:33:39 +0300

    2_567

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -10,7 +10,8 @@ class Question(models.Model):
     added_at = models.DateField(auto_now_add=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User,default=1)
-    likes = models.ManyToManyField(User, related_name='questions', blank=True)
+    #likes = models.ManyToManyField(User, related_name='questions', blank=True)
+    likes = models.TextField()
 
     def __unicode__(self):
         return self.title
